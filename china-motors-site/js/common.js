941bd34ce6aac53da17a0660054eff5b1c1e2715
Daniyal <daniyal@mail.com>
2026-02-09 22:18:39 +0500

    fix

--- china-motors-site/js/common.js
+++ china-motors-site/js/common.js
@@ -989,15 +989,16 @@
     const l = localStorage.getItem(LS_LANG);
     return translations[l] ? l : 'ru';
   }
-  
-  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-  (function(){
-    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-    s1.async=true;
-    s1.src='https://embed.tawk.to/68480dddb0d263190ae16f29/1itcncalg';
-    s1.charset='UTF-8';
-    s1.setAttribute('crossorigin','*');
-    s0.parentNode.insertBefore(s1,s0);
-  })();
 
+  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+  s1.async=true;
+  s1.src='https://embed.tawk.to/68480dddb0d263190ae16f29/1itcncalg';
+  s1.charset='UTF-8';
+  s1.setAttribute('crossorigin','*');
+  s0.parentNode.insertBefore(s1,s0);
+
+  
 })();
+
+
+
